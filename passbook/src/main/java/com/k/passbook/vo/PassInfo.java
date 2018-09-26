8c11fd574a81989a85b06d72d22565f67710a2c4
kaikanwu <wukaikan@gmail.com>
2018-09-26 12:32:30 +0800

    update vo

---
+++ passbook/src/main/java/com/k/passbook/vo/PassInfo.java
@@ -0,0 +1,26 @@
+package com.k.passbook.vo;
+
+
+import com.k.passbook.entity.Merchants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <h1>用户领取的优惠券信息</h1>
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassInfo {
+
+    /** 优惠券 */
+    private Pass pass;
+
+    /** 优惠券模板 */
+    private PassTemplate passTemplate;
+
+    /** 优惠券对应的商户 */
+    private Merchants merchants;
+}
